b2c373bdb289af593eb7a62e4b37f169eb16faad
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2017-05-19 13:55:18 +0200

    updated to new PluginInterface

--- iop_client_range_sensor_fkie/include/iop_client_range_sensor_fkie/RangeSensorClientPlugin_1_0.h
+++ iop_client_range_sensor_fkie/include/iop_client_range_sensor_fkie/RangeSensorClientPlugin_1_0.h
@@ -39,15 +39,8 @@ class DllExport RangeSensorClientPlugin_1_0 : public PluginInterface
 public:
 	RangeSensorClientPlugin_1_0();
 
-	JTS::Service* get_iop_service();
-	const std::type_info & get_iop_service_type();
-	const std::type_info & get_base_service_type();
-	void create_jts_service(JTS::JausRouter* jaus_router);
-	virtual ~RangeSensorClientPlugin_1_0();
-
-	jVariableLengthString get_service_uri() { return "urn:jaus:jss:environmentSensing:RangeSensorClient"; }
-	jUnsignedByte get_version_number_major() { return 1; }
-	jUnsignedByte get_version_number_minor() { return 0; }
+	JTS::Service* get_service();
+	void create_service(JTS::JausRouter* jaus_router);
 
 protected:
 	urn_jaus_jss_environmentSensing_RangeSensorClient_1_0::RangeSensorClientService *p_my_service;
